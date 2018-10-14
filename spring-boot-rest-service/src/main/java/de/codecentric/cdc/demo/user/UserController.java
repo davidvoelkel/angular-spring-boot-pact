5e17ec29a2766d3a0b14d776c8a4224cf4c53e8b
David Völkel <david.voelkel@codecentric.de>
2018-10-14 10:41:34 +0200

    Refactoring / simplifying

--- spring-boot-rest-service/src/main/java/de/codecentric/cdc/demo/user/UserController.java
+++ spring-boot-rest-service/src/main/java/de/codecentric/cdc/demo/user/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
@@ -14,7 +13,7 @@ public class UserController {
     @Autowired UserRepository userRepository;
 
     @RequestMapping("/user/{userName}")
-    public User user(@PathVariable String userName, HttpServletResponse response) throws IOException {
+    public User user(@PathVariable String userName) throws IOException {
         return userRepository.findUserByUserName(userName);
     }
 }
